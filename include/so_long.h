fccaca2a038ddc94692f50fff1093aea2007839b
j-torrez <juanboscotorrez@gmail.com>
2023-08-23 18:21:29 +0200

    Adding SRC dir and include dir which contain header file

---
+++ include/so_long.h
@@ -0,0 +1,13 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../../../MLX42/include/MLX42/MLX42.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <memory.h>
+
+# define WIDTH 256
+# define HEIGHT 256
+
+#endif

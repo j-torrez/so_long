59a65b6d07585f277c3c48f5a2611f60f654dd71
j-torrez <juanboscotorrez@gmail.com>
2023-08-28 19:22:22 +0200

    adding error handler

--- SRC/parse_map.c
+++ SRC/parse_map.c
@@ -6,9 +6,14 @@ char *read_map(char *map)
 	char *map_output;
 
 	fd = open(map, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("Unable to open file");
+		return NULL;
+	}
 	
-
 	map_output = get_next_line(fd);
+
 	close (fd);
 	return map_output; 	
 }

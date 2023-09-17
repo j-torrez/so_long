88918c90cc4627225d4614f5e8d086ad579ca983
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-09-17 05:55:03 +0200

    Fixing issue to add the Walls

--- SRC/initialize_struct.c
+++ SRC/initialize_struct.c
@@ -47,6 +47,7 @@ t_img	*initalize_img_struct (mlx_t *mlx)
 		ft_printf ("Error allocating memory for images asssets");
 
 	img_assets = load_sky_texture(mlx, img_assets);
+	img_assets = load_tree_texture(mlx, img_assets);
 
 	return img_assets;
 }
--- SRC/load_images.c
+++ SRC/load_images.c
@@ -19,17 +19,13 @@ t_img	*load_sky_texture(mlx_t *mlx, t_img *img)
 
 t_img	*load_tree_texture(mlx_t *mlx, t_img *img)
 {
-	//Try to load image file
 	mlx_texture_t *tree;
 	tree = mlx_load_png("./Sprites/bigtree.png");
 	if (!tree)
 		error_msg("Problem loading tree image");
-	//Convert texture to a displayable image
-	img->sky = mlx_texture_to_image(mlx, tree);
-	if (!img->sky)
+	img->tree = mlx_texture_to_image(mlx, tree);
+	if (!img->tree)
 		error_msg("Problem converting texture to image");
 	mlx_delete_texture(tree);
 	return (img);
-	//It return a pointer
-	// to a Structure ? So how do we access that element.
 }

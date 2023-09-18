fb18429fd727d607ba4105b732370f2718b341e5
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-09-18 20:09:58 +0200

    Initialize structure to store position of the character

--- SRC/initialize_struct.c
+++ SRC/initialize_struct.c
@@ -33,6 +33,10 @@ t_game *initialize_game_struct (char **grid)
 	game->width = width_map(grid);
 	game->height = height_map(grid);
 	game->grid = grid;
+	game->chara_pos_x = get_character_pos(game, 'x');
+	game->chara_pos_y = get_character_pos(game, 'y');
+	game->exit_pos_x = get_exit_pos(game, 'x');
+	game->exit_pos_y = get_exit_pos(game, 'y');
 
 	return (game);
 }

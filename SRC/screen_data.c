806eccc9282259d2cb38299864dcf28646189df2
j-torrez <juanboscotorrez@gmail.com>
2023-09-21 16:13:42 +0200

    Finishing error handling

--- SRC/screen_data.c
+++ SRC/screen_data.c
@@ -3,7 +3,8 @@
 void	string_to_screen(t_game *game)
 {
 	game->img->moves_print = mlx_put_string(game->mlx, "MOVES: ", 16, game->height * 64 - 44 );
-	mlx_image_to_window(game->mlx, game->img->honey, 140, game->height * 64 - 50);
+	if (mlx_image_to_window(game->mlx, game->img->honey, 140, game->height * 64 - 50) < 0)
+		error_msg("Error printing MOVE string in the window");
 	mlx_put_string (game->mlx, ":", 165, game->height * 64 -44);
 }
 
@@ -13,6 +14,7 @@ void	print_moves(t_game *game)
 	str = ft_itoa(game->steps + 1);
 	mlx_delete_image(game->mlx, game->img->moves_nbr);
 	game->img->moves_nbr = mlx_put_string(game->mlx, str, 80, game->height * 64 - 44);
+	ft_printf("Steps: %s\n", str);
 	free(str);
 }
 

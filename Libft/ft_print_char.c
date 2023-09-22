5d4eb5bbfa78abeac8cc842b62eb19438f3adee4
j-torrez <juanboscotorrez@gmail.com>
2023-09-22 11:55:39 +0200

    Fix indentation

--- Libft/ft_print_char.c
+++ Libft/ft_print_char.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_char.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: johnbosco <johnbosco@student.42.fr>        +#+  +:+       +#+        */
+/*   By: jtorrez- <jtorrez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:31:14 by johnbosco         #+#    #+#             */
-/*   Updated: 2023/06/14 17:05:20 by johnbosco        ###   ########.fr       */
+/*   Updated: 2023/09/22 11:49:29 by jtorrez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* The reason for returning 1 in this case is to indicate 
 the number of characters written successfully Dd.*/
 
-#include	"ft_printf.h"
+#include "ft_printf.h"
 
 int	ft_putchar(char c)
-{	
+{
 	write(1, &c, 1);
 	return (1);
 }
--- include/so_long.h
+++ include/so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtorrez- <jtorrez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 11:50:46 by jtorrez-          #+#    #+#             */
+/*   Updated: 2023/09/22 11:55:20 by jtorrez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -12,78 +24,78 @@
 
 typedef struct s_game
 {
-	char 		**grid;
-	int 		width;
-	int 		height;
-	int			steps;
-	int			collectibles;
-	int32_t		position_x;
-	int32_t		position_y;
-	mlx_t		*mlx;
-	int			chara_pos_x;
-	int			chara_pos_y;
-	int			exit_pos_x;
-	int			exit_pos_y;
-	struct s_img *img;
-	mlx_image_t *character;
-} t_game;
+	char			**grid;
+	int				width;
+	int				height;
+	int				steps;
+	int				collectibles;
+	int32_t			position_x;
+	int32_t			position_y;
+	mlx_t			*mlx;
+	int				chara_pos_x;
+	int				chara_pos_y;
+	int				exit_pos_x;
+	int				exit_pos_y;
+	struct s_img	*img;
+	mlx_image_t		*character;
+}	t_game;
 
 typedef struct s_map_content
 {
-	int exit;
-	int collectible;
-	int position;
+	int	exit;
+	int	collectible;
+	int	position;
 
-} t_map_content;
+}	t_map_content;
 
 typedef struct s_img
 {
-	mlx_image_t *sky;
-	mlx_image_t *tree;
-	mlx_image_t *honey;
-	mlx_image_t *character;
-	mlx_image_t *character_up;
-	mlx_image_t *character_down;
-	mlx_image_t *character_left;
-	mlx_image_t *character_right;
-	mlx_image_t *exit_close;
-	mlx_image_t *exit_open;
-	mlx_image_t *moves_print;
-	mlx_image_t *moves_nbr;
-	mlx_image_t *collectibles_nbr;
-	mlx_image_t *congrats_msg;
+	mlx_image_t	*sky;
+	mlx_image_t	*tree;
+	mlx_image_t	*honey;
+	mlx_image_t	*character;
+	mlx_image_t	*character_up;
+	mlx_image_t	*character_down;
+	mlx_image_t	*character_left;
+	mlx_image_t	*character_right;
+	mlx_image_t	*exit_close;
+	mlx_image_t	*exit_open;
+	mlx_image_t	*moves_print;
+	mlx_image_t	*moves_nbr;
+	mlx_image_t	*collectibles_nbr;
+	mlx_image_t	*congrats_msg;
 
-} t_img;
+}	t_img;
 
 
-char *read_map(char *map);
-char **map_as_array(char *map_one_line);
-void check_map_content(char *map_as_one_line);
-void check_map_empty (char *map_as_one_line);
-void validate_map_content (int i);
-void check_rectangular_map(char **map_as_arr);
-int width_map(char **map_as_array);
-int height_map(char **map_as_array);
-t_game *initialize_game_struct (char **grid);
-void check_walls_map(t_game *game);
-void error_msg(char *msg);
-t_game *initialize_data (char *map);
-void check_extension (char *word);
+char	*read_map(char *map);
+char	**map_as_array(char *map_one_line);
+void	check_map_content(char *map_as_one_line);
+void	check_map_empty(char *map_as_one_line);
+void	validate_map_content(int i);
+void	check_rectangular_map(char **map_as_arr);
+int		width_map(char **map_as_array);
+int		height_map(char **map_as_array);
+t_game	*initialize_game_struct(char **grid);
+void	check_walls_map(t_game *game);
+void	error_msg(char *msg);
+t_game	*initialize_data(char *map);
+void	check_extension(char *word);
 t_img	*load_sky_texture(mlx_t *mlx, t_img *img);
-t_img	*initalize_img_struct (mlx_t *mlx);
+t_img	*initalize_img_struct(mlx_t *mlx);
 void	background_image(t_game *data);
 t_img	*load_tree_texture(mlx_t *mlx, t_img *img);
-void render_map(t_game *data, int y, int x);
-void locate_images(t_game *data);
+void	render_map(t_game *data, int y, int x);
+void	locate_images(t_game *data);
 t_img	*load_collectible_texture(mlx_t *mlx, t_img *img);
 t_img	*load_main_character_texture(mlx_t *mlx, t_img *img);
-t_img *load_exit_close_texture(mlx_t *mlx, t_img *img);
-t_img *load_exit_open_texture(mlx_t *mlx, t_img *img);
-int	get_character_pos(t_game *game, char c);
-int	get_exit_pos(t_game *game, char c );
+t_img	*load_exit_close_texture(mlx_t *mlx, t_img *img);
+t_img	*load_exit_open_texture(mlx_t *mlx, t_img *img);
+int		get_character_pos(t_game *game, char c);
+int		get_exit_pos(t_game *game, char c );
 void	string_to_screen(t_game *game);
 void	print_moves(t_game *game);
-int	count_collectibles(t_game *game);
+int		count_collectibles(t_game *game);
 void	print_collectibles(t_game *game);
 void	remove_collectible(t_game *game, int y, int x);
 t_game	*move_up(t_game *game);
@@ -98,5 +110,5 @@ t_img	*load_character_up(mlx_t *mlx, t_img *img);
 t_img	*load_character_left(mlx_t *mlx, t_img *img);
 t_img	*load_character_down(mlx_t *mlx, t_img *img);
 t_img	*load_character_right(mlx_t *mlx, t_img *img);
-void check_game_status(t_game *game);
+void	check_game_status(t_game *game);
 #endif

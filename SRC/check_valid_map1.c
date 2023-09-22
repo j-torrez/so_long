7b366d997f1d1b7ccc5f2f700e374651bf356a78
j-torrez <juanboscotorrez@gmail.com>
2023-09-22 10:52:18 +0200

    Fixing indentation

--- SRC/check_valid_map1.c
+++ SRC/check_valid_map1.c
@@ -6,7 +6,7 @@
 /*   By: jtorrez- <jtorrez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 10:03:28 by jtorrez-          #+#    #+#             */
-/*   Updated: 2023/09/22 10:06:30 by jtorrez-         ###   ########.fr       */
+/*   Updated: 2023/09/22 10:49:55 by jtorrez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	check_rectangular_map(char **map_as_arr)
 {
-	size_t first_row;
-	size_t i;
+	size_t	first_row;
+	size_t	i;
 
 	first_row = ft_strlen(*map_as_arr);
 	i = 0;
@@ -28,26 +28,28 @@ void	check_rectangular_map(char **map_as_arr)
 		i++;
 	}
 }
+/* Check first and last column surronded by walls 
+Check first and last row surronded by walls*/
 
 void	check_walls_map(t_game *game)
 {
-	int i; 
+	int	i; 
 
 	i = 0;
-	// Check first and last column surronded by walls
 	while (i < game->height)
 	{
-		if ((game->grid[i][0] != '1') || (game->grid[i][game->width - 1] != '1') )
+		if ((game->grid[i][0] != '1') 
+		|| (game->grid[i][game->width - 1] != '1'))
 		{
 			error_msg("Map is not surronded by walls\n");
 		}
 		i++;
 	}
-	//Check first and last row surronded by walls
 	i = 0;
 	while (i < game->width)
 	{
-		if((game->grid[0][i]!= '1') || (game->grid[game->height - 1][i] != '1'))
+		if ((game->grid[0][i] != '1') || 
+		(game->grid[game->height - 1][i] != '1'))
 		{
 			error_msg("Map is not surronded by walls \n");
 		}
--- SRC/game_state.c
+++ SRC/game_state.c
@@ -6,30 +6,31 @@
 /*   By: jtorrez- <jtorrez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 10:03:49 by jtorrez-          #+#    #+#             */
-/*   Updated: 2023/09/22 10:06:29 by jtorrez-         ###   ########.fr       */
+/*   Updated: 2023/09/22 10:51:33 by jtorrez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void check_game_status(t_game *game)
+void	check_game_status(t_game *game)
 {
-	int total;
+	int	total;
 
 	total = count_collectibles(game);
-	
 	if (total == 0)
 	{
 		game->img->exit_close->enabled = false;
-		mlx_image_to_window(game->mlx, game->img->exit_open, game->exit_pos_x * PIXELS, game->exit_pos_y * PIXELS);
+		mlx_image_to_window(game->mlx, game->img->exit_open, 
+			game->exit_pos_x * PIXELS, game->exit_pos_y * PIXELS);
 		game->grid[game->exit_pos_y][game->exit_pos_x] = '0';
-		if (game->chara_pos_x == game->exit_pos_x && game->chara_pos_y == game->exit_pos_y)
+		if (game->chara_pos_x == game->exit_pos_x 
+			&& game->chara_pos_y == game->exit_pos_y)
 		{
-			game->img->congrats_msg = mlx_put_string(game->mlx, "CONGRATS ", game->width * 32, game->height * 32);
+			game->img->congrats_msg = mlx_put_string(game->mlx, "CONGRATS ", 
+					game->width * 32, game->height * 32);
 			sleep(1);
 			mlx_close_window(game->mlx);
 			ft_printf("CONGRATULATION!!\n YOU WON!!!!\n");
 		}
-		
 	}
 }

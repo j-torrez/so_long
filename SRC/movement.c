0c0317f86799a9e218ccb1eaf8deb541d9964601
j-torrez <juanboscotorrez@gmail.com>
2023-09-22 12:54:06 +0200

    Refactoring code

--- SRC/movement.c
+++ SRC/movement.c
@@ -6,7 +6,7 @@
 /*   By: jtorrez- <jtorrez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 10:05:06 by jtorrez-          #+#    #+#             */
-/*   Updated: 2023/09/22 12:46:22 by jtorrez-         ###   ########.fr       */
+/*   Updated: 2023/09/22 12:53:06 by jtorrez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,45 +107,4 @@ void	load_chara(t_game *game, char dir)
 	load_character_move_down(game, dir);
 	load_character_move_left(game, dir);
 	load_character_move_right(game, dir);
-}
-
-void	move_select(t_game *game, char line, char dir)
-{
-	if (line == 'y')
-	{
-		if (dir == 'u')
-			move_up(game);
-		else
-			move_down(game);
-	}
-	if (line == 'x')
-	{
-		if (dir == 'r')
-			move_right(game);
-		else
-			move_left(game);
-	}
-}
-
-void	move_player(t_game *game, char line, char dir)
-{
-	move_select(game, line, dir);
-	load_chara(game, dir);
-}
-
-void	move_hook(mlx_key_data_t keydata, void *data)
-{
-	t_game	*game;
-
-	game = (t_game *) data;
-	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(game->mlx);
-	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-		move_player(game, 'y', 'u');
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
-		move_player(game, 'x', 'r');
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-		move_player(game, 'y', 'd');
-	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		move_player(game, 'x', 'l');
 }
